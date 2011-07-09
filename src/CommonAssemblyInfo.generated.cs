ee990a673c4e3ca1a65c2eb84df759221c919b3a
lukesmith <stuff@lukesmith.net>
2011-07-09 11:07:53 +0100

    initial commit'

---
+++ src/CommonAssemblyInfo.generated.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("0.0.14.267")]
+[assembly: AssemblyProduct("BookUpon build-1.0.0.107-267-gc3535d5")]
+[assembly: AssemblyCopyright("copyright 2011, BookUpon")]
+[assembly: AssemblyTrademark("c3535d5ae95e085b1c512ea2a165908cf5ac7c9a")]
+[assembly: AssemblyVersion("0.0.14.0")]
+[assembly: AssemblyFileVersion("0.0.14.267")]
+
+[assembly: AssemblyInformationalVersion("0.0.14.0")]
+
